55e59c932a8e290969ba5c53089304d495ae250b
Christophe Wagner <chris.wagn22@gmail.com>
2019-01-19 13:09:23 +0700

    Improve CustomersList look

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,19 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import logo from "./logo.svg";
-import styles from "./App.css";
+import "./App.css";
 import CustomersList from "./CustomersList";
 import people from "./people.json";
 
 class App extends Component {
   render() {
-    return <div className={styles.App}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
+    return (
+      <div className={"App"}>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>YourTradebase Test</h1>
         <CustomersList customers={people} />
-      </div>;
+      </div>
+    );
   }
 }
 
--- src/components/CustomersList.js
+++ src/components/CustomersList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { getDistanceFromLatLonInKm } from "../utils/distance";
 import "./CustomersList.css";
 
@@ -6,7 +7,7 @@ class PrettyPrintJson extends React.Component {
   render() {
     const { data } = this.props;
     return (
-      <div>
+      <div className="PrettyPrintJson">
         <pre>{JSON.stringify(data, null, 2)}</pre>
       </div>
     );
@@ -24,7 +25,7 @@ class CustomersList extends Component {
         // People living in England
         if (customer.country !== "england") return false;
 
-        // People living within 100km of Bristol 
+        // People living within 100km of Bristol
         const distance = getDistanceFromLatLonInKm(
           bristollLat,
           bristollLong,
@@ -46,11 +47,19 @@ class CustomersList extends Component {
     };
 
     return (
-      <div className="Customers-list">
+      <div className="CustomersList">
+        <p>
+          There are {customersFiltered.length} customers living within 100km of
+          Bristol (GPS coordinates 51.450167, -2.594678) and living in England:
+        </p>
         <PrettyPrintJson data={customersFiltered} />
       </div>
     );
   }
 }
+
+CustomersList.propTypes = {
+  customers: PropTypes.array.isRequired
+};
 
 export default CustomersList;
